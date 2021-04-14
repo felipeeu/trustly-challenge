f5f9242809464fbffeac2dd8412b0ce154d203e8
Felipe Domingues <72865567+NucleoEF-1ano@users.noreply.github.com>
2021-04-14 09:46:03 -0300

    Create checkout component

---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import {Fragment} from  'react'
+import CheckoutDetails from "./components/checkout/details"
+
+const Checkout = () => {
+	return (
+		<Fragment>
+			<CheckoutDetails/>
+		</Fragment>
+	);
+};
+
+export default Checkout;
